dc448d8ff22f7375e7b0faea9b0caa9c7c6ee83c
Benjamin Graham <92594883+WiseBananaGaurdian@users.noreply.github.com>
2024-05-23 20:44:29 +0000

    Version 2
    Now with added support for special characters!

--- randompassword.py
+++ randompassword.py
@@ -1,14 +1,16 @@
 import random
 import string
 password = ("")
-string.ascii_letters
+special_chars = ("!", "@", "#", "$", "&", "*", "_", "-", ".", ",", ":", ";")
 PasswordLengthComparer = 0
 PasswordLength = int(input("What is the desired length of your password? >"))
 while PasswordLengthComparer < PasswordLength:
-	LetterOrNumber = random.randrange(0,2)
-	if LetterOrNumber == 0:
+	Character_Type = random.randrange(0,3)
+	if Character_Type == 0:
 		password += str(random.randint(0,9))
-	else:
+	elif Character_Type == 1:
 		password += random.choice(string.ascii_letters)
+	elif Character_Type == 2:
+		password += random.choice(special_chars)
 	PasswordLengthComparer += 1
 print(password)
